68719016b3db9ad2bf080388794cf05bb8873eeb
Ananya Gupta <50453232+ananya0504@users.noreply.github.com>
2020-09-28 23:46:59 +0530

    Add files via upload
    Day 11 of SQL

---
+++ day11.sql
@@ -0,0 +1,12 @@
+create table users(
+  id int(3),
+  name varchar(20),
+  age int(2)
+  );
+
+insert into users values(101,'John', 24);
+insert into users values(102,'Alex', 35);
+insert into users values(103,'Justin',18);
+
+ALTER TABLE users ADD PRIMARY KEY(id);
+ALTER TABLE Persons AUTO_INCREMENT=100;
